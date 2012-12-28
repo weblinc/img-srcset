192a5ec2f6f9864a7aa4bc15271e6222e0d0f82e
dknight <dknight@weblinc.com>
2012-12-28 01:07:48 -0500

    Added media-match submodule. Cleaned up directory structure to work with submodule. Cleaned up commented code in img.srcset. Added 'use strict'

--- img.srcset.js
+++ img.srcset.js
@@ -3,6 +3,8 @@
 
 // Imgsrcset
 (function(win) {
+    'use strict';
+
     var _srcsetl    = 0,
         _ratioExpr  = /\b[\d\.]+x\b/g,
         _srcExpr    = /[^\s]+/g,
@@ -36,11 +38,7 @@
 
                 this.srcsets.push({
                     element     : img,
-                    media       : srcset
-                                        /*.replace(/\b(\d+)w\b/g, '(max-width: $1)')
-                                        .replace(/\b(\d+)h\b/g, '(max-height: $1)')
-                                        .replace(/\b(\d+(\.\d*)?)x\b/g, '(resolution: $1dppx)')
-                                        .replace(/\)\s*\(/g, ') and (')*/,
+                    media       : srcset,
                     matches     : false
                 });
 
@@ -52,28 +50,28 @@
             parseMatch
         */
         parseMatch: function(srcset) {
-            //return false;
             var srcsetList  = srcset.replace(/\s*,\s*/, ',').split(','),
                 srcsetl     = srcsetList.length,
                 mql         = {src: '', media: '', matches: false},
                 match       = true;
 
             while (srcsetl--) {
-                var t   = srcsetList[srcsetl].match(/[^\s]+/g),
-                    tl  = t.length;
+                var list   = srcsetList[srcsetl].match(/[^\s]+/g),
+                    listl  = list.length;
 
                 match = true;
 
-                while (tl--) {
-                    if (tl === 0) {
-                        mql.src = t[tl];
+                while (listl--) {
+                    var item = list[listl];
+                    if (listl === 0) {
+                        mql.src = item;
                     } else {
-                        var prop        =   (t[tl].indexOf('w') !== -1 && 'width') || 
-                                            (t[tl].indexOf('h') !== -1 && 'height') || 
-                                            (t[tl].indexOf('x') !== -1 && 'resolution'),
-                            digits      = t[tl].match(/\d+/)[0],
-                            feature     = Media.features[prop],
-                            absValue    = Media.getAbsValue(digits + ((prop === 'resolution' && 'dppx') || ''));
+                        var prop        =   (item.indexOf('w') !== -1 && 'width') || 
+                                            (item.indexOf('h') !== -1 && 'height') || 
+                                            (item.indexOf('x') !== -1 && 'resolution'),
+                            digits      = item.match(/\d+/)[0],
+                            feature     = win.Media.features[prop],
+                            absValue    = win.Media.getAbsValue(digits + ((prop === 'resolution' && 'dppx') || ''));
 
                         if (!(match = ((prop === 'resolution' && feature === absValue) || (prop !== 'resolution' && feature <= absValue)))) {
                             break;
@@ -100,12 +98,12 @@
                 var id = _srcsetl;
 
                 do {
-                    var srcset  = Imgsrcset.srcsets[id],
+                    var srcset  = win.Imgsrcset.srcsets[id],
                         match   = false;
 
                     if (typeof srcset === 'undefined') { continue; }
 
-                    match = Imgsrcset.parseMatch(srcset.media);
+                    match = win.Imgsrcset.parseMatch(srcset.media);
 
                     if (match && !(srcset.matches === match.media)) {
                         srcset.matches = match.media;
