49857a2951efde37f8201803fb8f8446d264ca12
David Knight <dknight@weblinc.com>
2013-01-31 10:39:11 -0500

    Update to version 2.0.0

--- img.srcset.js
+++ img.srcset.js
@@ -1,135 +1,153 @@
 /* imgsrcset - Img srcset polyfill for resolution responsive images. Authors & copyright (c) 2012: WebLinc, David Knight. */
-/* NOTE: Depends on Media object. See https://github.com/weblinc/media-match */
 
 // Imgsrcset
 (function(win) {
     'use strict';
 
-    var _srcsetl    = 0,
-        _ratioExpr  = /\b[\d\.]+x\b/g,
-        _srcExpr    = /[^\s]+/g,
-        _timer      = 0;
-
-    /*
-        imgsrcset
-    */
-    win.Imgsrcset = {
-        // Properties
-        srcsets: [],
-
-        // Methods
+    var _viewport       = win.document.documentElement,
+        _srcsetID       = 0,
+        _srcsets        = [],
+        _eventPrefix    = '',
+        _addEvent       = win.addEventListener || (_eventPrefix = 'on') && win.attachEvent,
+        _removeEvent    = win.removeEventListener || win.detachEvent,
+        _srcExpr        = /[^\s]+/g,
+        _digitExpr      = /[0-9\.]+/g,
+        _timer          = 0,
 
         /*
-            parse
+            _matches
         */
-        parse: function() {
-            this.srcsets = [];
+        _matches    = function(srcset) {
+            var srcList     = (srcset.indexOf(',') !== -1 && srcset.split(',')) || [srcset],
+                srcIndex    = srcList.length - 1,
+                srcLength   = srcIndex,
 
-            var imgs    = win.document.getElementsByTagName('img') || [],
-                imgsl   = imgs.length - 1;
+                list        = null,
+                listIndex   = 0,
 
-            do {
-                var img     = imgs[imgsl],
-                    srcset  = img.getAttribute('srcset') || '';
+                src         = '',
+                media       = '';
 
-                if (!srcset) {
-                    continue;
-                }
-
-                this.srcsets.push({
-                    element     : img,
-                    media       : srcset,
-                    matches     : false
-                });
-
-                _srcsetl++;
-            } while(imgsl--);
-        },
-
-        /*
-            parseMatch
-        */
-        parseMatch: function(srcset) {
-            var srcsetList  = srcset.replace(/\s*,\s*/, ',').split(','),
-                srcsetl     = srcsetList.length,
-                mql         = {src: '', media: '', matches: false},
-                match       = true;
-
-            while (srcsetl--) {
-                var list   = srcsetList[srcsetl].match(/[^\s]+/g),
-                    listl  = list.length;
-
-                match = true;
-
-                while (listl--) {
-                    var item = list[listl];
-                    if (listl === 0) {
-                        mql.src = item;
-                    } else {
-                        var prop        =   (item.indexOf('w') !== -1 && 'width') || 
-                                            (item.indexOf('h') !== -1 && 'height') || 
-                                            (item.indexOf('x') !== -1 && 'resolution'),
-                            digits      = item.match(/\d+/)[0],
-                            feature     = win.Media.features[prop],
-                            absValue    = win.Media.getAbsValue(digits + ((prop === 'resolution' && 'dppx') || ''));
-
-                        if (!(match = ((prop === 'resolution' && feature === absValue) || (prop !== 'resolution' && feature <= absValue)))) {
-                            break;
-                        }
-                    }
-                }
-
-                if (match) {
-                    mql.media = srcsetList[srcsetl];
-                    mql.matches = match;
-                }
+            if (srcIndex < 0) {
+                return;
             }
 
-            return (mql.matches && mql) || false;
+            do {
+                var list        = srcList[srcLength - srcIndex].match(_srcExpr) || [],
+                    listIndex   = list.length;
+
+                while (listIndex--) {
+                    var item    = list[listIndex],
+                        feature = 0,
+                        digits  = 0;
+
+                    if (listIndex > 0) {
+                        feature =   (item.indexOf('w') !== -1 && (win.innerWidth || _viewport.clientWidth)) || 
+                                    (item.indexOf('h') !== -1 && (win.innerHeight ||  _viewport.clientHeight)) || 
+                                    (item.indexOf('x') !== -1 && (win.devicePixelRatio || 1));
+
+                        digits  = Number(item.match(_digitExpr));
+
+                        if (feature && digits && digits > feature) {
+                            break;
+                        }
+                    } else {
+                        src     = item;
+                        media   = srcList[srcIndex];
+                    }
+                }
+            } while (srcIndex--);
+
+            return (src && media && {src: src, media: media}) || false;
         },
 
         /*
             watch
         */
-        watch: function(evt) {
+        _watch      = function(evt) {
             clearTimeout(_timer);
 
             _timer = setTimeout(function() {
-                var id = _srcsetl;
+                var srcset          = null,
+                    srcsetIndex     = _srcsetID - 1,
+                    srcsetLength    = srcsetIndex,
+                    match           = false;
 
                 do {
-                    var srcset  = win.Imgsrcset.srcsets[id],
-                        match   = false;
+                    srcset = _srcsets[srcsetLength - srcsetIndex];
 
-                    if (typeof srcset === 'undefined') { continue; }
+                    // If img element does not have a parent, remove array index to prevent caching
+                    if (!srcset.element.parentNode) {
+                        _srcsetID--;
+                        srcset.splice(srcsetIndex, 1);
+                        continue;
+                    }
 
-                    match = win.Imgsrcset.parseMatch(srcset.media);
+                    match = _matches(srcset.media);
 
-                    if (match && !(srcset.matches === match.media)) {
+                    if (match && (srcset.matches !== match.media)) {
                         srcset.matches = match.media;
 
-                        srcset.element.setAttribute('src', match.src);
+                        srcset.element.src = match.src;
                     } else if (!match) {
                         srcset.matches = false;
+                        srcset.src && (srcset.element.src = srcset.src);
                     }
-                } while(id--);
+                } while(srcsetIndex--);
             }, 10);
         },
 
         /*
             init
         */
-        init: function() {
+        _init        = function() {
+            _removeEvent(_eventPrefix + 'load', _init);
+
             win.Imgsrcset.parse();
-            win.Imgsrcset.watch();
+            _watch();
 
-            Media.listen(win.Imgsrcset.watch);
+            // Processes '_srcsets' array and determines which source to use
+            // '_watch' will clear out any images from the array that do not have parents, which should eliminate element caching
+            _addEvent(_eventPrefix + 'resize', _watch);
+            _addEvent(_eventPrefix + 'orientationchange', _watch);
+        };
+
+    /*
+        imgsrcset
+    */
+    win.Imgsrcset = {
+        /*
+            parse
+            
+            Called on '_init' and can also be called if new images are added/removed
+         */
+        parse: function() {
+            _srcsets = [];
+
+            var imgs        = win.document.getElementsByTagName('img') || [],
+                imgIndex    = imgs.length - 1,
+                imgLength   = imgIndex,
+                img         = null,
+                srcset      = '';
+
+            do {
+                img     = imgs[imgLength - imgIndex];
+                srcset  = img.getAttribute('srcset') || '';
+
+                if (!srcset) {
+                    continue;
+                }
+
+                _srcsetID = _srcsets.push({
+                    element     : img,
+                    media       : srcset,
+                    matches     : false,
+                    src         : img.getAttribute('src') || ''
+                });
+            } while(imgIndex--);
         }
-    }
+    };
 
-    if (win.addEventListener) {
-        win.addEventListener('load', win.Imgsrcset.init);
-    } else {
-        win.attachEvent('onload', win.Imgsrcset.init);
-    }
+    // Set up listeners
+    _addEvent(_eventPrefix + 'load', _init);
 })(window);
